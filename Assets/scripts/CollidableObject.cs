5422275063283699cf993453bbba34f2a85e4d80
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-02-09 19:23:35 +0900

    オブジェクトの当たり判定用スクリプト追加

---
+++ Assets/scripts/CollidableObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collidable : MonoBehaviour
+{
+    private Collider2D z_Collider;
+    [SerializeField]
+    private ContactFilter2D z_Filter;
+    private List<Collider2D> z_CollidedObjects = new List<Collider2D>(1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        z_Collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(z_Filter);
+        Debug.Log(z_CollidedObjects);
+        z_Collider.OverlapCollider(z_Filter, z_CollidedObjects);
+        
+        foreach(var obj in z_CollidedObjects)
+        {
+            Debug.Log("Collided with " + obj.name);
+        }
+    }
+}
