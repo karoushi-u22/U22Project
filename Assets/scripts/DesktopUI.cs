91500ac75055d43f7b9151707f4aa9d437c6d67f
tomo3101 <tacstomo.sub@gmail.com>
2024-02-29 13:08:40 +0900

    feature: アイテムの有無、不正なアイテムの有無によって表示するUI画像を変更する機能を追加

--- Assets/scripts/DesktopUI.cs
+++ Assets/scripts/DesktopUI.cs
@@ -7,15 +7,23 @@ using U22Game.Events;
 namespace U22Game.UI{
     public class DesktopUI : MonoBehaviour
     {
-        public Image imageToShow;
-        private bool isImageVisible = false; // 画像の表示状態を管理するフラグ
-        [SerializeField]
-        private CharacterController2D characterController; // キャラクターの移動を制御するスクリプトへの参照
+        [SerializeField] private Image deskPC;
+        [SerializeField] private Image usbImage;
+        [SerializeField] private Image stickyNoteImage;
+        [SerializeField] private Image softwareImage;
+
+        [SerializeField] private Sprite badusbSprite;
+        [SerializeField] private Sprite badsoftwareSprite;
+
+        [SerializeField] private CharacterController2D characterController; // キャラクターの移動を制御するスクリプトへの参照
 
         private void Start()
         {
             // 初期状態では画像を非表示にする
-            SetImageVisibility(false);
+            SetImageVisibility(deskPC, false);
+            SetImageVisibility(usbImage, false);
+            SetImageVisibility(stickyNoteImage, false);
+            SetImageVisibility(softwareImage, false);
 
             // DesktopEventスクリプトから発生するイベントを購読
             DesktopEvent.OnItemGenerated += HandleItemGenerated;
@@ -30,8 +38,21 @@ namespace U22Game.UI{
         // イベントハンドラー: アイテムが生成されたときに呼ばれる
         private void HandleItemGenerated(string objectName, DesktopHandler desktopHandler)
         {
+            // 不正なアイテムがある場合、対応する画像を設定する
+            if (desktopHandler.isBadUsb)
+            {
+                usbImage.sprite = badusbSprite;
+            }
+            else if (desktopHandler.isBadSoftware)
+            {
+                softwareImage.sprite = badsoftwareSprite;
+            }
+
             // 画像を表示する
-            SetImageVisibility(true);
+            SetImageVisibility(deskPC, true);
+            SetImageVisibility(usbImage, desktopHandler.existsUsb);
+            SetImageVisibility(stickyNoteImage, desktopHandler.isBadStickyNote);
+            SetImageVisibility(softwareImage, desktopHandler.wasInstalledSoftware);
 
             // キャラクターの移動を制御するスクリプトを一時的に無効にする
             characterController.enabled = false;
@@ -40,18 +61,20 @@ namespace U22Game.UI{
         private void Update()
         {
             // ESCキーが押され、かつ画像が表示されている場合に画像を非表示にする
-            if (Input.GetKeyDown(KeyCode.Escape) && isImageVisible)
+            if (Input.GetKeyDown(KeyCode.Escape) && deskPC.gameObject.activeSelf)
             {
-                SetImageVisibility(false);
+                SetImageVisibility(deskPC, false);
+                SetImageVisibility(usbImage, false);
+                SetImageVisibility(stickyNoteImage, false);
+                SetImageVisibility(softwareImage, false);
                 characterController.enabled = true;
             }
         }
 
         // 画像の表示/非表示を切り替えるメソッド
-        private void SetImageVisibility(bool isVisible)
+        private void SetImageVisibility(Image image, bool isVisible)
         {
-            imageToShow.gameObject.SetActive(isVisible);
-            isImageVisible = isVisible;
+            image.gameObject.SetActive(isVisible);
         }
     }
 }
