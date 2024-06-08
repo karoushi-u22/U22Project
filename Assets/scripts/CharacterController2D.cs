071f67ff8699403a31566b9a9cec050ed114ce07
S4mecode <ohs20108@ohs.hal.ac.jp>
2024-06-08 20:14:55 +0900

    add:キャラクターアニメーションの追加 以下同文

--- Assets/scripts/CharacterController2D.cs
+++ Assets/scripts/CharacterController2D.cs
@@ -1,38 +1,59 @@
-using UnityEngine;
-using U22Game.Handlers;
+    using UnityEngine;
+    using U22Game.Handlers;
 
-namespace U22Game.Controller{
-    public class CharacterController2D : MonoBehaviour
-    {
-        public float moveSpeed = 2f; // キャラクターの移動速度
-
-        private Rigidbody2D rb;
-
-        void Start()
+    namespace U22Game.Controller{
+        public class CharacterController2D : MonoBehaviour
         {
-            rb = GetComponent<Rigidbody2D>();
-        }
+            public float moveSpeed = 2f; // キャラクターの移動速度
 
-        // Update is called once per frame
-        void Update()
-        {
-            Vector2 movementInput = InputHandler.GetMovementInput(); // 入力を取得
-
-            // 単方向のみの入力に制限する
-            if (Mathf.Abs(movementInput.x) > 0 && Mathf.Abs(movementInput.y) > 0)
+            private Rigidbody2D rb;
+            private Animator animator;
+            
+            void Start()
             {
-                if (Mathf.Abs(movementInput.x) > Mathf.Abs(movementInput.y))
+                rb = GetComponent<Rigidbody2D>();
+                animator = GetComponent<Animator>();
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                Vector2 movementInput = InputHandler.GetMovementInput(); // 入力を取得
+
+                // 単方向のみの入力に制限する
+                if (Mathf.Abs(movementInput.x) > 0 && Mathf.Abs(movementInput.y) > 0)
                 {
-                    movementInput.y = 0f;
+                    if (Mathf.Abs(movementInput.x) > Mathf.Abs(movementInput.y))
+                    {
+                        movementInput.y = 0f;
+                    }
+                    else
+                    {
+                        movementInput.x = 0f;
+                    }
+                }
+
+                // 移動ベクトルに速度を掛けて移動させる
+                rb.velocity = movementInput * moveSpeed;
+
+                // アニメーションの設定
+                if (movementInput != Vector2.zero)
+                {
+                    animator.SetFloat("Horizontal", movementInput.x);
+                    animator.SetFloat("Vertical", movementInput.y);
+                    animator.SetBool("IsMoving", true);
+                    animator.enabled = true;
                 }
                 else
                 {
-                    movementInput.x = 0f;
+                    animator.SetBool("IsMoving", false);
+                    animator.enabled = false;
                 }
             }
-
-            // 移動ベクトルに速度を掛けて移動させる
-            rb.velocity = movementInput * moveSpeed;
         }
     }
-}
+
+
+
+
+                      
