38b21840988352bd8aad32e66497bb6c51f1f6ea
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-07-01 02:50:27 +0900

    add: Jsonから読み込んだイベントの選択肢をボタンで表示する処理を追加

--- Assets/scripts/Handler/EventTextLoader.cs
+++ Assets/scripts/Handler/EventTextLoader.cs
@@ -12,8 +12,13 @@ namespace U22Game.Handlers
         private static CharacterController2D characterController;
         private readonly string characterGameobject = "main_character_1";
         private TextboxHandler textboxHandler;
+        private EventSelection eventSelection;
+        private EventSelection.PlayerSelection.Body.Selection selectChoice;
+        private string jsonFileName;
         private bool startTextboxFlag = false;
         private bool onClickFlag = false;
+        private bool onComplateSetTextFlag = false;
+        private bool onClickButtonFlag = false;
 
         void Awake()
         {
@@ -27,15 +32,21 @@ namespace U22Game.Handlers
                 Destroy(gameObject);
             }
 
+            instance.eventSelection = JsonIoHandler.LoadSelecttionFromJson();
+
             // キャラクターコントローラーの取得
             characterController = GameObject.Find(characterGameobject).GetComponent<CharacterController2D>();
+
+            // TextboxHandlerのイベントを使用
+            TextboxHandler.StartTextboxEvent += StartTextbox;
         }
 
         void Start()
         {
             // TextboxHandlerのイベントを使用
-            TextboxHandler.StartTextboxEvent += StartTextbox;
             TextboxHandler.TextboxClickEvent += OnClickTextbox;
+            TextboxHandler.CompleteSetTextEvent += OnCompleteSetText;
+            SelectButtonHandler.ClickEvent += OnClickButton;
         }
 
         void OnDestroy()
@@ -43,6 +54,7 @@ namespace U22Game.Handlers
             // TextboxHandlerのイベントを解除
             TextboxHandler.StartTextboxEvent -= StartTextbox;
             TextboxHandler.TextboxClickEvent -= OnClickTextbox;
+            TextboxHandler.CompleteSetTextEvent -= OnCompleteSetText;
         }
 
         static public void LoadEvent(string jsonFile)
@@ -55,6 +67,8 @@ namespace U22Game.Handlers
             {
                 Debug.Log($"Sender: {eventText.sender}, Text: {eventText.text}");
             }
+
+            instance.jsonFileName = jsonFile;  // イベントのJsonファイル名を取得
 
             // イベントテキストを順に表示
             instance.StartCoroutine(instance.DisplayEventText(eventTextList));
@@ -75,11 +89,43 @@ namespace U22Game.Handlers
                 foreach (var eventText in eventTextList)
                 {
                     onClickFlag = false;
+                    onComplateSetTextFlag = false;
 
-                    textboxHandler.SetTextMain(eventText.text); // メインテキストを設定
+                    textboxHandler.SetTextMain(eventText.text, eventText.delay); // メインテキストを設定
                     textboxHandler.SetTextPlayerName(eventText.sender); // プレイヤー名を設定
 
-                    yield return new WaitUntil(() => onClickFlag == true);
+                    if (eventText.selections != null)
+                    {
+                        yield return new WaitUntil(() => onComplateSetTextFlag == true);  // テキストが全て表示されるまで待機
+
+                        SelectButtonHandler.ShowButton(eventText.selections);
+
+                        yield return new WaitUntil(() => onClickButtonFlag == true);  // いずれかの選択肢のボタンが押されるまで待機
+
+                        EventSelection.PlayerSelection playerSelection = new() { title = instance.jsonFileName + " : " + eventText.text };  // 選択肢を格納するインスタンスを新規作成
+
+                        foreach (var selection in eventText.selections)  // 各選択肢を取り出し、選択肢の配列に追加
+                        {
+                            if (selectChoice.title.Equals(selection))  // プレイヤーが選んだ選択肢を追加
+                            {
+                                playerSelection.body.selections.Add(selectChoice);
+                            }
+                            else  // プレイヤーが選ばなかった選択肢を追加
+                            {
+                                playerSelection.body.selections.Add(new EventSelection.PlayerSelection.Body.Selection{
+                                    title = selection,
+                                    playerSelected = false
+                                });
+                            }
+                        }
+
+                        eventSelection.playerSelections.Add(playerSelection);  // 選択肢を格納したインスタンスを選択肢の配列に追加
+                        JsonIoHandler.SaveSelectionsToJson(eventSelection);  // 選択肢をJsonに保存
+                    }
+                    else
+                    {
+                        yield return new WaitUntil(() => onClickFlag == true);
+                    }
                 }
 
                 textboxHandler.HideTextbox(); // テキストボックスを非表示
@@ -102,6 +148,19 @@ namespace U22Game.Handlers
         void OnClickTextbox()
         {
             onClickFlag = true;
+        }
+
+        // テキストが全て表示された時のイベント
+        void OnCompleteSetText()
+        {
+            onComplateSetTextFlag = true;
+        }
+
+        // 選択肢のボタンが押された時のイベント
+        void OnClickButton(EventSelection.PlayerSelection.Body.Selection selection)
+        {
+            onClickButtonFlag = true;
+            instance.selectChoice = selection;
         }
     }
 }
