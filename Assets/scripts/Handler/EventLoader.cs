0ddac516158479c550469ce04b583e6ea8cbd7e2
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-07-01 17:23:09 +0900

    fix: 同タイミングに実行するイベントが複数ある場合、イベントを一つずつ実行するように修正

--- Assets/scripts/Handler/EventLoader.cs
+++ Assets/scripts/Handler/EventLoader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,17 @@ namespace U22Game.Handlers
         [SerializeField] private string jsonFile = "events.json";
         private List<Events.Event> eventList;
         private int currentDate;
+        private bool isFinishEvent = true;
+
+        void Awake()
+        {
+            EventTextLoader.FinishEvent += OnFinishEvent;
+        }
+
+        void OnDestroy()
+        {
+            EventTextLoader.FinishEvent -= OnFinishEvent;
+        }
 
         void Start()
         {
@@ -16,20 +28,34 @@ namespace U22Game.Handlers
                 eventList = Events.Event.FromJson(jsonFile);
                 currentDate = JsonIoHandler.LoadFromJson().CurrentDate;
 
-                foreach (var eventItem in eventList)
-                {
-                    Debug.Log($"EventFile: {eventItem.eventFile}\nExecuteDate: {eventItem.executeDate}\nExecuteTiming: {eventItem.executeTiming}");
-
-                    if (eventItem.executeDate == currentDate && eventItem.executeTiming.Equals("afterLoad"))
-                    {
-                        EventTextLoader.LoadEvent(eventItem.eventFile);
-                    }
-                }
+                StartCoroutine(ExecuteEvents(eventList));
             }
             else
             {
                 Debug.LogError("JSON file not assigned.");
             }
+        }
+
+        void OnFinishEvent()
+        {
+            isFinishEvent = true;
+        }
+
+        private IEnumerator ExecuteEvents(List<Events.Event> eventList)
+        {
+            foreach (var eventItem in eventList)
+                {
+                    Debug.Log($"EventFile: {eventItem.eventFile}\nExecuteDate: {eventItem.executeDate}\nExecuteTiming: {eventItem.executeTiming}");
+
+                    if (eventItem.executeDate == currentDate && eventItem.executeTiming.Equals("afterLoad"))
+                    {
+                        yield return new WaitUntil(() => isFinishEvent);
+
+                        EventTextLoader.LoadEvent(eventItem.eventFile);
+
+                        isFinishEvent = false;
+                    }
+                }
         }
     }
 }
